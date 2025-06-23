4a9e4c740362c46af8c6f704ff778473e5e17019
P J Anthony <104015990+pjanthony2001@users.noreply.github.com>
2025-06-23 21:06:02 +0200

    Fix bug

--- api/contact.rs
+++ api/contact.rs
@@ -15,6 +15,16 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn handler(req: Request) -> Result<Response<Body>, Error> {
+
+    if req.method() == "OPTIONS" {
+        return Ok(Response::builder()
+            .status(204)
+            .header("Access-Control-Allow-Origin", "*") // or restrict to your frontend origin
+            .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+            .header("Access-Control-Allow-Headers", "*")
+            .body(Body::Empty)?);
+    }
+
     let body = req.body();
     let contact: ContactForm = serde_json::from_slice(body)?;
 
@@ -27,7 +37,7 @@ async fn handler(req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
         .status(200)
         .header("Content-Type", "application/json")
-        .header("Access-Control-Allow-Origin", "*") // frontend URL here
+        .header("Access-Control-Allow-Origin", "*")
         .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         .body(Body::Empty)?)
 }
